d8af393cb519070c23caf3fc289358a976b250a2
Sheikh Sayed <sksayed@live.com>
2019-05-05 14:19:50 +0600

    all done

--- InheritenceTesting/Program.cs
+++ InheritenceTesting/Program.cs
@@ -15,7 +15,59 @@ namespace InheritenceTesting
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app!
+            Rectangle r = new Rectangle();
         }
     }
+
+    class Shape
+    {
+        protected double height;
+        protected double width;
+
+        public Shape()
+        {
+            this.height = 0;
+            this.width = 0;
+            Console.WriteLine(" base shape constructor has been made ");
+
+        }
+
+        public void setHeight(double height)
+        {
+            this.height = height;
+        }
+
+        public void setWidth(double weidth)
+        {
+            this.height = weidth;
+        }
+
+        public double getHeight()
+        {
+            return this.height;
+        }
+        public double getWeidth()
+        {
+            return this.width;
+        }
+
+        protected double GetArea()
+        {
+            return this.width * this.height;
+        }
+
+
+    }
+
+    class Rectangle : Shape
+    {
+        public Rectangle()
+        {
+            Console.WriteLine(" inside the child rectangle class ");
+        }
+    }
+
+    
+
 }
